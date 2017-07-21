a6f6e2c875a14784c309b9e1fbf3961e648ee228
augustin <au@gmail.com>
2017-07-21 09:28:57 +0200

    add contact form

--- sendemail.php
+++ sendemail.php
@@ -6,7 +6,7 @@
     header( 'Content-type: application/json' );
 
 
-    $to         = 'trendytheme.net@gmail.com';  // put your email here
+    $to         = 'augustin.meignan@gmail.com';  // put your email here
 
     $email_template = 'simple.html';
 
